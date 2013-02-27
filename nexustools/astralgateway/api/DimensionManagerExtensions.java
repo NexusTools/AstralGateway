dcc5a2ea7c72f11095bcce2bdd9d3d20764e34b9
aerospark <lukefay12@gmail.com>
2013-02-27 12:09:35 -0500

    Fixed DimensionManagerExtensions

--- nexustools/astralgateway/api/DimensionManagerExtensions.java
+++ nexustools/astralgateway/api/DimensionManagerExtensions.java
@@ -6,23 +6,11 @@ package nexustools.astralgateway.api;
 
 import java.util.ArrayList;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityList;
 import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.network.packet.Packet70GameEvent;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.stats.AchievementList;
 import net.minecraft.util.ChunkCoordinates;
-import net.minecraft.util.MathHelper;
 import net.minecraft.world.Teleporter;
-import net.minecraft.world.WorldManager;
-import net.minecraft.world.WorldProvider;
 import net.minecraft.world.WorldServer;
-import net.minecraft.world.WorldServerMulti;
-import net.minecraft.world.WorldSettings;
-import net.minecraft.world.storage.ISaveHandler;
 import net.minecraftforge.common.DimensionManager;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.world.WorldEvent;
 
 /**
  *
@@ -32,102 +20,71 @@ public class DimensionManagerExtensions {
 
     static ArrayList<Integer> loaded = new ArrayList<Integer>();
 
-    public static void transferEntityToDimension(EntityPlayerMP ent, int dim) { // will likely replace customInitDimension
+    public static void transferEntityToDimension(EntityPlayerMP ent, int dim) {
         final WorldServer pass = DimensionManager.getWorld(dim);
-        ent.mcServer.getConfigurationManager().transferPlayerToDimension(ent, dim, new Teleporter(pass){
-        
-            @Override
-            public boolean func_85188_a/*  generatePortal?  */(Entity ent) {
-//                        ent.setLocationAndAngles(0, 250, 0, 0, 0);
-                int xr = 0;
-                int yr = 40;
-                int zr = 0;
-                for (int x = xr - 10; x < xr + 10; x++) {
-                    for (int y = yr - 10; y < yr + 10; y++) {
-                        for (int z = zr - 10; z < zr + 10; z++) {
-                            if (x == xr && y == yr - 8 && z == zr) {
-                                pass.setBlock(x, y, z, 4002);
-                            } else {
-                                pass.setBlock(x, y, z, 0);
+        if(dim == 0)
+            ent.mcServer.getConfigurationManager().transferPlayerToDimension(ent, dim, new Teleporter(pass){
+                
+                @Override
+                public boolean placeInExistingPortal(Entity ent, double par2, double par4, double par6, float par8) {
+                    ChunkCoordinates c = pass.getSpawnPoint();
+                    if(c == null){ // sadfaic
+                        ent.setVelocity(0,0,0);
+                        ent.setLocationAndAngles(0, pass.getHeightValue(0,0)+4, 0, 0, 0);
+                        System.out.println("Moved to 0,0 (spawnpoint is null!)");
+                    }else{
+                        ent.setVelocity(0,0,0);
+                        ent.setLocationAndAngles(c.posX, pass.getHeightValue(c.posX,c.posZ)+4, c.posZ, 0, 0);
+                        System.out.println("Moved to " + c.posX + ", " + c.posY + ", " + c.posZ);
+                    }
+                    return true;
+                }
+
+                @Override
+                public void placeInPortal(Entity ent, double par2, double par4, double par6, float par8) {
+                    ChunkCoordinates c = pass.getSpawnPoint();
+                    if(c == null){ // sadfaic
+                        ent.setVelocity(0,0,0);
+                        ent.setLocationAndAngles(0, pass.getHeightValue(0,0)+4, 0, 0, 0);
+                        System.out.println("Moved to 0,0 (spawnpoint is null!)");
+                    }else{
+                        ent.setVelocity(0,0,0);
+                        ent.setLocationAndAngles(c.posX, pass.getHeightValue(c.posX,c.posZ)+4, c.posZ, 0, 0);
+                        System.out.println("Moved to " + c.posX + ", " + c.posY + ", " + c.posZ);
+                    }
+                }
+                
+            }); // no idea if this will cause issues with the nether gate (creating an invalid return point)
+        else
+            ent.mcServer.getConfigurationManager().transferPlayerToDimension(ent, dim, new Teleporter(pass){
+
+                @Override
+                public boolean func_85188_a/*  generatePortal?  */(Entity ent) {
+                    int xr = 0;
+                    int yr = 40;
+                    int zr = 0;
+                    for (int x = xr - 10; x < xr + 10; x++) {
+                        for (int y = yr - 10; y < yr + 10; y++) {
+                            for (int z = zr - 10; z < zr + 10; z++) {
+                                if (x == xr && y == yr - 8 && z == zr) {
+                                    pass.setBlock(x, y, z, 4002);
+                                } else {
+                                    pass.setBlock(x, y, z, 0);
+                                }
                             }
                         }
                     }
-                }
-                for (int y = yr; y < 255; y++) {
-                    pass.setBlock(xr, y, zr, 0);
+                    for (int y = yr; y < 255; y++) {
+                        pass.setBlock(xr, y, zr, 0);
+                    }
+
+                    ent.setVelocity(0,0,0);
+                    ent.setLocationAndAngles(xr, yr+1, zr, 0, 0);
+
+                    return true; // finally.... no more automatic nethergate spawning
                 }
 
-                ent.setLocationAndAngles(xr, yr, zr, 0, 0);
-
-                return true; // finally.... no more automatic nethergate spawning
-            }
-        
-        });
+            });
     }
  
-//    public static void customInitDimension(int dim) { // small amount of code from forge here, but they didnt implement a way to make a custom teleporter so...
-//
-//        if (loaded.contains(dim)) {
-//            return;
-//        }
-//
-//        WorldServer overworld = DimensionManager.getWorld(0);
-//        if (overworld == null) {
-//            throw new RuntimeException("Cannot Hotload Dim: Overworld is not Loaded!");
-//        }
-//
-//        try {
-//            DimensionManager.getProviderType(dim);
-//        } catch (Exception e) {
-//            System.err.println("Cannot Hotload Dim: " + e.getMessage());
-//            return; //If a provider hasn't been registered then we can't hotload the dim
-//        }
-//        MinecraftServer mcServer = overworld.getMinecraftServer();
-//        ISaveHandler savehandler = overworld.getSaveHandler();
-//        WorldSettings worldSettings = new WorldSettings(overworld.getWorldInfo());
-//
-//        WorldServer world = (dim == 0 ? overworld : new WorldServerMulti(mcServer, savehandler, overworld.getWorldInfo().getWorldName(), dim, worldSettings, overworld, mcServer.theProfiler) {
-//            @Override
-//            public Teleporter func_85176_s/*  getTeleporter?  */() {
-//                final WorldServer pass = this;
-//                return new Teleporter(this) {
-//                    @Override
-//                    public boolean func_85188_a/*  generatePortal?  */(Entity ent) {
-////                        ent.setLocationAndAngles(0, 250, 0, 0, 0);
-//                        int xr = 0;
-//                        int yr = 40;
-//                        int zr = 0;
-//                        for (int x = xr - 10; x < xr + 10; x++) {
-//                            for (int y = yr - 10; y < yr + 10; y++) {
-//                                for (int z = zr - 10; z < zr + 10; z++) {
-//                                    if (x == xr && y == yr - 8 && z == zr) {
-//                                        pass.setBlock(x, y, z, 4002);
-//                                    } else {
-//                                        pass.setBlock(x, y, z, 0);
-//                                    }
-//                                }
-//                            }
-//                        }
-//                        for (int y = yr; y < 255; y++) {
-//                            pass.setBlock(xr, y, zr, 0);
-//                        }
-//
-//                        ent.setLocationAndAngles(xr, yr, zr, 0, 0);
-//
-//                        return true; // finally.... no more automatic nethergate spawning
-//                    }
-//                };
-//            }
-//        });
-//        world.addWorldAccess(new WorldManager(mcServer, world));
-//        MinecraftForge.EVENT_BUS.post(new WorldEvent.Load(world));
-//        if (!mcServer.isSinglePlayer()) {
-//            world.getWorldInfo().setGameType(mcServer.getGameType());
-//        }
-//
-//        mcServer.setDifficultyForAllWorlds(mcServer.getDifficulty());
-//
-//        loaded.add(dim);
-//
-//    }
 }
