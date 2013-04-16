327208d203966ec0c3160637f56a4bfe55f67401
aerospark <lukefay12@gmail.com>
2013-04-16 16:05:31 -0400

    Fixed InstancedSimplex

--- nexustools/terrain/InstancedSimplex.java
+++ nexustools/terrain/InstancedSimplex.java
@@ -1,10 +1,6 @@
 package nexustools.terrain;
 
-import simplex.*;
-import java.lang.reflect.Field;
 import java.util.Random;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
